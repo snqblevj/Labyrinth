dd924cedb61863576fa50a39068b4aa7113e0690
Theo Lorio <leniglochoukar@gmail.com>
2021-05-06 15:18:40 +0200

    Ajout de la classe mère Piece

---
+++ src/composants/PieceM0.java
@@ -0,0 +1,30 @@
+package composants;
+
+/**
+ *
+ * Cette classe permet de reprÃ©senter les piÃ¨ces du jeu de modÃ¨le 0.
+ *
+ */
+public class PieceM0 extends Piece {
+
+    /**
+     * A Faire (Quand Qui Statut)
+     *
+     * Constructeur permettant de construire une piÃ¨ce de modÃ¨le 0 et d'orientation 0.
+     */
+    public PieceM0() {
+        // A Modifier !!!
+        super(-1,false,false,false,false);
+    }
+    /**
+     * A Faire (Quand Qui Statut)
+     *
+     * MÃ©thode permettant de crÃ©er une copie de la piÃ¨ce (un nouvelle objet Java).
+     * @return Une copie de la piÃ¨ce.
+     */
+    public Piece copy(){
+        Piece piece=null;
+        // A ComplÃ©ter
+        return piece;
+    }
+}
